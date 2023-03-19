f3e15c1d8fed6eb7637a3f7ebc02efff50425274
Nick" Byung heon Lee <43138813+smartbh@users.noreply.github.com>
2023-03-19 17:35:37 +0900

    25083

---
+++ 25083/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // \과 "는 특수 문자열이므로 출력하기 위해선
+    // 앞에 \를 한개 더 붙여줘야한다
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+    return 0;
+}
