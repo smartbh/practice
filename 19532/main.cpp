c79f136671545129c0283317d8dac231b629c9c4
Nick" Byung heon Lee <43138813+smartbh@users.noreply.github.com>
2023-03-28 16:33:32 +0900

    19532

---
+++ 19532/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f = 0;
+
+    cin >> a >>b >>c >>d >>e >> f;
+
+
+    for(int i = -999; i<1000; i++)
+    {
+        for(int j = -999; j<1000; j++)
+        {
+            if(((a*i + b * j) == c )&&((d*i + e*j) == f))
+                cout << i << j;
+        }
+    }
+}
